2f5cbdef6fafa5f2f49a2c14833518336cc9601e
eCituk <citukcaamal@gmail.com>
2024-06-25 23:20:09 -0500

    Configurar modelos en Eloquent

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,48 +2,17 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Base\User
 {
-    use HasFactory, Notifiable;
-
-    protected $primaryKey = 'user_id';
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function newEloquentBuilder($query): Builders\UserBuilder
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return new Builders\UserBuilder($query);
+    }
+    public function courses(): HasMany
+    {
+        return $this->hasMany(Course::class, 'owner_id', 'user_id');
     }
 }
