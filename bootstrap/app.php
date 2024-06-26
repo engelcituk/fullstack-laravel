342ba50fd0f3e879c9891455a089e9df1f8e00c1
eCituk <citukcaamal@gmail.com>
2024-06-26 00:57:09 -0500

    Ajustar la url de redirección del usuario en base a sus datos, middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        // $middleware->appendToGroup('backoffice', [...]);
+        $middleware->redirectTo(
+            users: fn() => auth()->user()->getRedirectUrl(),
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
