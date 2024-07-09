38875b722e41ec3d9bcca541035b362679bb5224
eCituk <citukcaamal@gmail.com>
2024-07-08 23:16:42 -0500

    Crear component Laravel BakofficeLayout

---
+++ app/View/Components/BackofficeLayout.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class BackofficeLayout extends Component
+{
+    /**
+     * php artisan make:component BackofficeLayout
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('layouts.backoffice');
+    }
+}
