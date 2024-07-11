bbaf9f0703e42bbe4bffc15ff73a1f90a0609866
eCituk <citukcaamal@gmail.com>
2024-07-10 23:14:35 -0500

    Publicar BackofficeLayout de Vue para que se pueda usar en blade

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,10 +1,13 @@
 
 
 <script>
+import { defineAsyncComponent } from "vue";
+
+const BackofficeLayout = defineAsyncComponent( () => import('@/layouts/BackofficeLayout.vue') )
 export default {
     name: 'App',
     components: {
-
+        BackofficeLayout,
     },
     setup() {
         return {
