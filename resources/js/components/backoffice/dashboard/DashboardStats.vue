ba99a30898c94919ced264135eea1b1229462df5
eCituk <citukcaamal@gmail.com>
2024-07-12 23:24:45 -0500

    Renderizado de componentes de stats dinamico con datos del backend

---
+++ resources/js/components/backoffice/dashboard/DashboardStats.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'DashboardStats'
+}
+</script>
+
+<script setup>
+import useComponentsLoader from "@/composables/useComponentsLoader.js";
+
+const props = defineProps({
+    stats: {
+        type: Array,
+        required: true
+    }
+})
+
+const { findComponentsByName, components, statsLoader  } = useComponentsLoader()
+const loadedComponents = components( props.stats, statsLoader )
+</script>
+
+
+<template>
+    <component
+        v-for="stat in stats"
+        :key="stat.id"
+        :is="findComponentsByName(stat.component, loadedComponents)"
+        v-bind="stat"
+    />
+</template>
